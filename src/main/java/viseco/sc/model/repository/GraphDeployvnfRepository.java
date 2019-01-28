35e82fc247fc9e8c99f654e3bb3f41f217f5f037
Abdul Wahab <abdul.wahab@fokus.fraunhofer.de>
2019-01-28 10:29:00 +0100

    new Model Of Component

--- src/main/java/viseco/sc/model/repository/GraphDeployvnfRepository.java
+++
@@ -1,9 +0,0 @@
-package viseco.sc.model.repository;
-
-import com.mongodb.Mongo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import viseco.sc.model.document.Component;
-import viseco.sc.model.document.GraphDeploy;
-
-public interface GraphDeployvnfRepository extends MongoRepository<GraphDeploy,String> {
-}
--- src/main/java/viseco/sc/model/repository/ServiceGraphsrRepository.java
+++
@@ -1,8 +0,0 @@
-package viseco.sc.model.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import viseco.sc.model.document.ServiceGraphs;
-
-
-public interface ServiceGraphsrRepository extends MongoRepository<ServiceGraphs,String> {
-}
